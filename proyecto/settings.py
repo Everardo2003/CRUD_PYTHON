9640c36d9b6fd50bde62ce50b2e1c06a973952dc
Everardo <everardodiazmartinez04@gmail.com>
2025-09-20 16:55:47 -0600

    Mejoras

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -25,8 +25,6 @@ SECRET_KEY = 'django-insecure-d61^4lra90i)crs-ema_gm2xqa_-jhuj5wdgszwmzxiy!@_ec$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -122,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
